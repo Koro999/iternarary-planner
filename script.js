4528f842f8c6400cce8d17e8a301cd0cd9bd18f2
mahmud khan deep <mahmudkhan_deep@yahoo.com>
2023-06-01 17:24:57 -0400

    modified

--- script.js
+++ script.js
@@ -1,145 +1,77 @@
-// Working search bar, that interacts with the google API (Carlos)
-// -location is picked. and information can be pulled
-var searchedCity = $('.input');
-var screenMap = $('#map-container');
-var carlosKey = 'AIzaSyBfijwlDdGDJ2LiGaA6IL5b1wOf0a1PPsE';
+let currentCollection = 1;
+let cardId = 1;
 
-var map;
-var service;
+function addCard() {
+  const title = document.getElementById('cardTitleInput').value;
+  const content = document.getElementById('cardContentInput').value;
 
-window.searchPlace = function (city) {
-    var geocodeApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${carlosKey}&address=${city}`;    //api url to search for a location's latlng
-    var placeLat;
-    var placeLon;
+  const card = createCardElement(title, content);
 
-    fetch(geocodeApiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP status ${response.status} occurred while fetching data.`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.status === 'OK') { //only if the input is a valid location address, its latitude and longitude will be stored in variable
-                placeLat = data.results[0].geometry.location.lat;
-                placeLon = data.results[0].geometry.location.lng;
-                var cityLatLng = new google.maps.LatLng(placeLat, placeLon);
+  const cardContainer = document.getElementById('cardContainer');
+  cardContainer.appendChild(card);
 
-                map = new google.maps.Map(document.getElementById('map-container'), { center: cityLatLng, zoom: 15 });    //creates a new Map object, and displayed in #map-container in html
+  document.getElementById('cardTitleInput').value = '';
+  document.getElementById('cardContentInput').value = '';
 
-                var request = {
-                    location: cityLatLng,
-                    radius: '500',     //radius of location in m
-                    types: ['tourist_attraction'] //enter a specified type of location. visit https://developers.google.com/maps/documentation/javascript/supported_types to see a list of supported place types.
-                };
-                service = new google.maps.places.PlacesService(map);    //  creates a new instance of the PlacesService object provided by the Google Maps Places library, and associating it with the map
-                service.nearbySearch(request, callback);    // calls 'nearbySearch' method on the 'PlacesService' instance
-            }
-        })
-        .catch(error => console.error(error));
-};
-
-function callback(results, status) {
-
-    //If the status of the Places API request is OK, it logs the name of each place in the console. If the status is not OK, it logs a message saying that no results were returned.
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            console.log(place.name);
-        }
-    } else {
-        console.log('Place search did not return any results.');
-    }
+  cardId++;
 }
 
-$('.search').on('click', () => {
-    searchPlace(searchedCity[0].value);
-})
+function createCardElement(title, content) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.id = `card-${cardId}`;
 
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = title;
 
-$(document).ready(function () {
+  const contentElement = document.createElement('p');
+  contentElement.textContent = content;
 
-    // Assigns an on click event to the dropdown button
-    $(document).on('click', '.Itenerary .dropdown-trigger button', function (event) {
-        event.stopPropagation();
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.onclick = () => editCard(card.id);
 
-        var allActiveIteneraries = $(".Itenerary.is-active");   // stores all active Itenerary elements in variable
-        var currentItenerary = $(this).closest('.Itenerary');   // goes through clicked element's parents that matches 'Itenerary'
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.onclick = () => deleteCard(card.id);
 
-        // Remove 'is-active' class from all active Iteneraries that aren't the current one]
-        allActiveIteneraries.each(function () {  //.each() loops all active itenerary queries
-            if (!$(this).is(currentItenerary)) {    // checks if if the 'currentItenerary' is not the same as the element in current iteration
-                $(this).removeClass('is-active');
-            }
-        });
+  card.appendChild(titleElement);
+  card.appendChild(contentElement);
+  card.appendChild(editButton);
+  card.appendChild(deleteButton);
 
-        currentItenerary.toggleClass('is-active'); // Toggle the current dropdown
-    });
-});
-
-
-// Function to handle the edit action for a card
-function handleEditCard(event) {
-  // Get the card element
-  const cardElement = event.target.closest('.card');
-
-  // Perform the edit action
-  // For example, you can enable editing of the card's content
+  return card;
 }
 
-// Function to handle the delete action for a card
-function handleDeleteCard(event) {
-  // Get the card element
-  const cardElement = event.target.closest('.card');
+function editCard(cardId) {
+  const card = document.getElementById(cardId);
+  const titleElement = card.querySelector('h3');
+  const contentElement = card.querySelector('p');
 
-  // Perform the delete action
-  // For example, you can remove the card from the collection
-  cardElement.remove();
+  const newTitle = prompt('Enter a new title:', titleElement.textContent);
+  const newContent = prompt('Enter new content:', contentElement.textContent);
+
+  titleElement.textContent = newTitle;
+  contentElement.textContent = newContent;
 }
 
-// Function to handle the save action for the current collection of cards
-function handleSaveCollection() {
-  // Get the collection of cards
-  const cardCollection = document.querySelectorAll('.card');
-
-  // Perform the save action
-  // For example, you can store the card data in a database or local storage
+function deleteCard(cardId) {
+  const card = document.getElementById(cardId);
+  card.remove();
 }
 
-// Function to handle navigation between sets of cards
-function handleNavigation(event) {
-  // Get the navigation button element
-  const navigationButton = event.target;
+function saveCollection() {
+  const collectionSelect = document.getElementById('collectionSelect');
+  const selectedCollection = collectionSelect.value;
 
-  // Get the target set of cards based on the button's data attribute
-  const targetSet = navigationButton.dataset.targetSet;
-
-  // Perform the navigation action
-  // For example, you can hide/show the corresponding set of cards
-  const cardSets = document.querySelectorAll('.card-set');
-  cardSets.forEach((cardSet) => {
-      cardSet.classList.toggle('hidden', cardSet.id !== targetSet);
-  });
+  // Code to save the collection goes here...
+  alert(`Collection ${selectedCollection} saved!`);
 }
 
-// Add event listeners to the "Edit" buttons
-const editButtons = document.querySelectorAll('.edit-button');
-editButtons.forEach((editButton) => {
-  editButton.addEventListener('click', handleEditCard);
-});
+function changeCollection() {
+  const collectionSelect = document.getElementById('collectionSelect');
+  const selectedCollection = collectionSelect.value;
 
-// Add event listeners to the "Delete" buttons
-const deleteButtons = document.querySelectorAll('.delete-button');
-deleteButtons.forEach((deleteButton) => {
-  deleteButton.addEventListener('click', handleDeleteCard);
-});
-
-// Add event listener to the "Save Collection" button
-const saveButton = document.querySelector('.save-button');
-saveButton.addEventListener('click', handleSaveCollection);
-
-// Add event listeners to the navigation buttons
-const navigationButtons = document.querySelectorAll('.navigation-button');
-navigationButtons.forEach((navigationButton) => {
-  navigationButton.addEventListener('click', handleNavigation);
-});
+  // Code to switch to the selected collection goes here...
+  alert(`Switched to Collection ${selectedCollection}!`);
+}
