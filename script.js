43d10ae0735f19e71ea900c24bf8b933255339d2
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-06-01 20:15:00 -0400

    changed the location wher the card is created on the page

--- script.js
+++ script.js
@@ -1,80 +1,108 @@
 
 let currentCollection = 1;
-let cardId = 1;
+let cardId = -1; //changed from 1 to -1
 
 function addCard() {
-  const title = document.getElementById('cardTitleInput').value;
-  const content = document.getElementById('cardContentInput').value;
+    const title = document.getElementById('cardTitleInput').value;
+    const content = document.getElementById('cardContentInput').value;
 
-  const card = createCardElement(title, content);
+    const card = createCardElement(title, content);
 
-  const cardContainer = document.getElementById('cardContainer');
-  cardContainer.appendChild(card);
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.appendChild(card);
 
-  document.getElementById('cardTitleInput').value = '';
-  document.getElementById('cardContentInput').value = '';
+    document.getElementById('cardTitleInput').value = '';
+    document.getElementById('cardContentInput').value = '';
 
-  cardId++;
+    cardId++;
 }
 
 function createCardElement(title, content) {
-  const card = document.createElement('div');
-  card.className = 'card';
-  card.id = `card-${cardId}`;
 
-  const titleElement = document.createElement('h3');
-  titleElement.textContent = title;
+    var iteneraryCardsParent = $('#Itenerary3Cards');
+    var cardContainer = $('<div class="dropdown-content"></div>');
+    iteneraryCardsParent.append(cardContainer);
 
-  const contentElement = document.createElement('p');
-  contentElement.textContent = content;
+    var IteneraryCardDiv = $(`<div id="IteneraryCard${cardId}" class="dropdown-item button IteneraryCardsBtn card"></div>`);
+    cardContainer.append(IteneraryCardDiv);
 
-  const editButton = document.createElement('button');
-  editButton.textContent = 'Edit';
-  editButton.onclick = () => editCard(card.id);
+    var cardTitle = $(`<h3>${title}</h3>`);
+    IteneraryCardDiv.append(cardTitle);
 
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-  deleteButton.onclick = () => deleteCard(card.id);
+    var cardP = $(`<p>${content}</p>`);
+    IteneraryCardDiv.append(cardP);
 
-  card.appendChild(titleElement);
-  card.appendChild(contentElement);
-  card.appendChild(editButton);
-  card.appendChild(deleteButton);
+    var editButton = $('<button></button>'); // Creates a new button element
+    editButton.text('Edit'); // Sets the text content of the button
+    editButton.on('click', () => editCard(card.id)); // Sets the click event handler of the button
+    IteneraryCardDiv.append(editButton);
 
-  return card;
+    var deleteButton = $('<button></button>'); // Creates a new button element
+    deleteButton.text('Delete'); // Sets the text content of the button
+    deleteButton.on('click', () => deleteCard(card.id)); // Sets the click event handler of the button
+    IteneraryCardDiv.append(deleteButton);
+
+
+
+
+
+    //   const card = document.createElement('div');
+    //   card.className = 'card';
+    //   card.id = `card-${cardId}`;
+
+    //   const titleElement = document.createElement('h3');
+    //   titleElement.textContent = title;
+
+    //   const contentElement = document.createElement('p');
+    //   contentElement.textContent = content;
+
+    //   const editButton = document.createElement('button');
+    //   editButton.textContent = 'Edit';
+    //   editButton.onclick = () => editCard(card.id);
+
+    //   const deleteButton = document.createElement('button');
+    //   deleteButton.textContent = 'Delete';
+    //   deleteButton.onclick = () => deleteCard(card.id);
+
+    //   card.appendChild(titleElement);
+    //   card.appendChild(contentElement);
+    //   card.appendChild(editButton);
+    //   card.appendChild(deleteButton);
+
+    return iteneraryCardsParent;
 }
 
 function editCard(cardId) {
-  const card = document.getElementById(cardId);
-  const titleElement = card.querySelector('h3');
-  const contentElement = card.querySelector('p');
+    const card = document.getElementById(cardId);
+    const titleElement = card.querySelector('h3');
+    const contentElement = card.querySelector('p');
 
-  const newTitle = prompt('Enter a new title:', titleElement.textContent);
-  const newContent = prompt('Enter new content:', contentElement.textContent);
+    const newTitle = prompt('Enter a new title:', titleElement.textContent);
+    const newContent = prompt('Enter new content:', contentElement.textContent);
 
-  titleElement.textContent = newTitle;
-  contentElement.textContent = newContent;
+    titleElement.textContent = newTitle;
+    contentElement.textContent = newContent;
 }
 
 function deleteCard(cardId) {
-  const card = document.getElementById(cardId);
-  card.remove();
+    const card = document.getElementById(cardId);
+    card.remove();
 }
 
 function saveCollection() {
-  const collectionSelect = document.getElementById('collectionSelect');
-  const selectedCollection = collectionSelect.value;
+    const collectionSelect = document.getElementById('collectionSelect');
+    const selectedCollection = collectionSelect.value;
 
-  // Code to save the collection goes here...
-  alert(`Collection ${selectedCollection} saved!`);
+    // Code to save the collection goes here...
+    alert(`Collection ${selectedCollection} saved!`);
 }
 
 function changeCollection() {
-  const collectionSelect = document.getElementById('collectionSelect');
-  const selectedCollection = collectionSelect.value;
+    const collectionSelect = document.getElementById('collectionSelect');
+    const selectedCollection = collectionSelect.value;
 
-  // Code to switch to the selected collection goes here...
-  alert(`Switched to Collection ${selectedCollection}!`);
+    // Code to switch to the selected collection goes here...
+    alert(`Switched to Collection ${selectedCollection}!`);
 }
 
 // Working search bar, that interacts with the google API (Carlos)
